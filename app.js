a1a027d34b931577cd5b77ab7b3b657567f1743a
kgonjo <kgonjo@yahoo-corp.jp>
2018-05-02 10:46:48 +0900

    Git init

---
+++ app.js
@@ -0,0 +1,92 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'ssshhhhh'}));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+var sess;
+
+app.get('/',function(req,res){
+    sess = req.session;
+//Session set when user Request our app via URL
+    if(sess.email) {
+        /*
+        * This line check Session existence.
+        * If it existed will do some action.
+        */
+        res.redirect('/admin');
+    }
+    else {
+        res.render('index');
+    }
+});
+
+app.post('/login',function(req,res){
+    sess = req.session;
+//In this we are assigning email to sess.email variable.
+//email comes from HTML page.
+    sess.email=req.body.email;
+    res.end('done');
+});
+
+app.get('/admin',function(req,res){
+    sess = req.session;
+    if(sess.email) {
+        res.write(' <h1>Hello '+sess.email+'</h1>');
+        res.end('<a href="+">Logout</a>');
+    } else {
+        res.write(' <h1>Please login first.</h1>');
+        res.end('<a href="+">Login</a>');
+    }
+});
+
+app.get('/logout',function(req,res){
+    req.session.destroy(function(err) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.redirect('/');
+        }
+    });
+
+});
+
+// app.use('/', indexRouter);
+// app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
